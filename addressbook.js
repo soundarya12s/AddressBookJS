416b425a31408895ac6594a52adac6957873d4a2
S Soundarya <soundarya.s@tvsnext.com>
2023-09-15 22:46:50 +0530

    [Soundarya]Added

--- addressbook.js
+++ addressbook.js
@@ -47,38 +47,51 @@ const Contact2 = new Contact(
     Zip= "123456",
     PhoneNumber= "1023445678",
     Email= "swetha@gmail.com");
-    function validateName(Details) {
-        console.log(Details.FirstName + " " + Details.LastName + " " + Details.Address + " " + Details.City 
+
+    function validate(Details) {
+    console.log(Details.FirstName + " " + Details.LastName + " " + Details.Address + " " + Details.City
         + " " + Details.State + " " + Details.Zip + " " + Details.PhoneNumber + " " + Details.Email);
-        if (!/^[A-Z][a-zA-Z]{2,}$/.test(Details.FirstName)) {
-            throw new Error("Invalid First Name");
-        }
-        else if (!/^[A-Z][a-zA-Z]{2,}$/.test(Details.LastName)) {
-            throw new Error("Invalid Last Name");
-        }
-        else if (!/^[0-9a-zA-Z]{4,}$/.test(Details.Address)) {
-            throw new Error("Invalid Address");
-        }
-        else if (!/^[a-zA-Z]{4,}$/.test(Details.City)) {
-            throw new Error("Invalid City");
-        }
-        else if (!/^[0-9a-zA-Z]{4,}$/.test(Details.State)) {
-            throw new Error("Invalid State");
-        }
-        else if (!/^[0-9]{6}$/.test(Details.Zip)) {
-            throw new Error("Invalid Zip");
-        }
-        else if (!/^[0-9]{10}$/.test(Details.PhoneNumber)) {
-            throw new Error("Invalid Phone Number");
-        }
-        else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(Details.Email)) {
-            throw new error("Invalid Email")
-        }
-        else {
-            addressbook.addNewContact(Details);
-        }
+    if (!/^[A-Z][a-zA-Z]{2,}$/.test(Details.FirstName)) {
+        throw new Error("Invalid First Name");
     }
+    else if (!/^[A-Z][a-zA-Z]{2,}$/.test(Details.LastName)) {
+        throw new Error("Invalid Last Name");
+    }
+    else if (!/^[0-9a-zA-Z]{4,}$/.test(Details.Address)) {
+        throw new Error("Invalid Address");
+    }
+    else if (!/^[a-zA-Z]{4,}$/.test(Details.City)) {
+        throw new Error("Invalid City");
+    }
+    else if (!/^[0-9a-zA-Z]{4,}$/.test(Details.State)) {
+        throw new Error("Invalid State");
+    }
+    else if (!/^[0-9]{6}$/.test(Details.Zip)) {
+        throw new Error("Invalid Zip");
+    }
+    else if (!/^[0-9]{10}$/.test(Details.PhoneNumber)) {
+        throw new Error("Invalid Phone Number");
+    }
+    else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(Details.Email)) {
+        throw new error("Invalid Email")
+    }
+    else {
+        addressbook.addNewContact(Details);
+    }
+}
     
-    validateName(Contact1);
-    validateName(Contact2)
-    addressbook.displayContactDetails();
+validate(Contact1);
+validate(Contact2);
+
+const Contact3 = new Contact(
+    FirstName = "Shri",
+    LastName = "Mathi",
+    Address = "Anna Nagar",
+    City = "Chennai",
+    State = "TN",
+    Zip = "123456",
+    PhoneNumber = "1023445678",
+    Email = "Shri@gmail.com");
+validate(Contact3);
+
+addressbook.displayContactDetails();
